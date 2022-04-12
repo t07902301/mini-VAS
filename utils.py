0cb07ac1b4070ec540124331ca06cc5ac5906c11
yiwei <e-well@outlook.com>
2022-04-11 20:18:24 -0600

    plots with titles, 1M data retrivial

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import numpy as np
 class proximity:
     def __init__(self,point_set,set_eps=False) -> None:
-        if set_eps:
+        if set_eps==False:
             self.epsilon=3.2147064340016937e-07
+            # self.epsilon=3.797682913998397e-07 #2000
+            # self.epsilon=1.8988414569991985e-07
         else:
             self.epsilon=self.set_epsilon(point_set)
     def set_epsilon(self,point_set):
@@ -19,6 +21,7 @@ class proximity:
             i+=1
         # return max_norm/100
         return np.power(max_norm/100,2)*2
+        # return np.power(max_norm/100,2)
     def run(self,point_1,point_2):
         return np.exp(-np.power(np.linalg.norm(point_1-point_2),2)/self.epsilon)    
         # return     np.linalg.norm(point_1-point_2)/self.epsilon
@@ -31,3 +34,11 @@ class proximity:
     #     except:
     #         print(point_1,point_2)
     #         exit(0)
+def get_obj(samples,prox):
+    obj=0
+    size=samples.shape[0]
+    for i in range(size-1):
+        for j in range(i+1,size):
+            obj+=prox.run(samples[i],samples[j])    
+            # obj+=proximity(samples[i],samples[j])
+    return np.around(obj,2)
